db01c9c2074ddabff97f3328c307426011eec9dc
khojimatov <khojimatov14@gmail.com>
2025-06-14 19:50:07 +0500

    commit

--- data/config.py
+++ data/config.py
@@ -1,7 +1,11 @@
+import os
 from environs import Env
 
 env = Env()
 env.read_env()
+
+if os.getenv("RAILWAY_ENVIRONMENT") is None:  # faqat lokalda ishlasa
+    env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
