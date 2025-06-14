5af0a19781282d84a86de52c9cde01023ee65e1e
khojimatov <khojimatov14@gmail.com>
2025-06-14 19:58:31 +0500

    commit

--- data/config.py
+++ data/config.py
@@ -2,10 +2,7 @@ import os
 from environs import Env
 
 env = Env()
-env.read_env()
-
-if os.getenv("production") is None:
-    env.read_env()
+# env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
