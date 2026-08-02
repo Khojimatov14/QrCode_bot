0d599f1d87dc771bcf1d60e94488818f64d1a489
khojimatov <khojimatov14@gmail.com>
2026-08-02 10:58:35 +0500

    Commit

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,6 +4,7 @@ from aiogram import types
 async def set_default_commands(bot):
     await bot.set_my_commands(
         [
-            types.BotCommand(command='start', description='Botni ishga tushurish')
+            types.BotCommand(command='start', description='Botni ishga tushurish'),
+            types.BotCommand(command='bot', description='Yangi botga buyurtma berish!')
         ]
     )
